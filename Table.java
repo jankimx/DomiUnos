3cfd96b14ddabd8a9b24dd7311bf340d3fa09ee0
jankimx <36344199+jankimx@users.noreply.github.com>
2018-04-23 00:14:16 -0400

    made table better for game
    made sure that boolean is returned so that dominos are removed from player's hand when possible

--- Table.java
+++ Table.java
@@ -1,64 +1,75 @@
 public class Table {
-	Dominoes [] dominoes = new Dominoes [54];
-	Tile right;
-	Tile left;
-	int numDoms = 0;
-	int rightSide = 28;
-	int leftSide  = 26;
-	
-	//Dominoes currently on table, not in order
-	public boolean addOnTable (Dominoes toAdd) {
-		if (numDoms == 0) {
-		dominoes[27] = toAdd;
-		right = dominoes[27].botside;
-		left = dominoes[27].topside;
-		numDoms++;
-		return true;
-		} else { //If the dominoes topside is equal to the outmost right tile, then we add that
-				// domino on the right side of the array and make right equal to botside
-				// we run the same idea on the other 3 cases
-			if (toAdd.topside == right) {
-				dominoes[rightSide] = toAdd;
-				right = toAdd.botside;
-				rightSide++;
-				numDoms++;
-				return true;
-			}
-			else if(toAdd.botside == right) {
-				dominoes[rightSide] = toAdd;
-				right = toAdd.topside;
-				rightSide++;
-				numDoms++;
-				return true;
-			}
-			else if(toAdd.topside == left) {
-				dominoes[leftSide] = toAdd;
-				left = toAdd.botside;
-				leftSide--;
-				numDoms++;
-				return true;
-			}
-			else if(toAdd.botside == left) {
-				dominoes[leftSide] = toAdd;
-				right = toAdd.topside;
-				leftSide--;
-				numDoms++;
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-	}
-	
-	public String toString () {
-		
-		String toReturn = "";
-		for (int i = leftSide; i < rightSide; i++) {
-			toReturn += " " + dominoes[i];
-		}
-		
-		return toReturn;
-	}
-	
+    Dominoes [] dominoes = new Dominoes [54];
+    Tile right;
+    Tile left;
+    int numDoms = 0;
+    int rightSide = 28;
+    int leftSide  = 26;
+     
+    //Dominoes currently on table, not in order
+    public boolean addOnTable (Dominoes toAdd) {
+        if (numDoms == 0) {
+        dominoes[27] = toAdd;
+        right = dominoes[27].botside;
+        left = dominoes[27].topside;
+        numDoms++;
+        return true;
+        } else { //If the dominoes topside is equal to the outmost right tile, then we add that
+                // domino on the right side of the array and make right equal to botside
+                // we run the same idea on the other 3 cases
+            if (toAdd.topside == right) {
+                dominoes[rightSide] = toAdd;
+                right = toAdd.botside;
+                rightSide++;
+                numDoms++;
+                return true;
+            }
+            else if(toAdd.botside == right) {
+                dominoes[rightSide] = toAdd;
+                right = toAdd.topside;
+                rightSide++;
+                numDoms++;
+                return true;
+            }
+            else if(toAdd.topside == left) {
+                dominoes[leftSide] = toAdd;
+                left = toAdd.botside;
+                leftSide--;
+                numDoms++;
+                return true;
+            }
+            else if(toAdd.botside == left) {
+                dominoes[leftSide] = toAdd;
+                right = toAdd.topside;
+                leftSide--;
+                numDoms++;
+                return true;
+            }
+            else {
+            	return false;
+            }
+        }
+    }
+     
+//  public void addOnTable (Dominoes toAdd) {
+//      if (numDoms == 0) {
+//          dominoes[27] = toAdd;
+//          numDoms ++;
+//      }
+//      else {
+//      dominoes[rightSide] = toAdd;
+//      rightSide ++;
+//      numDoms ++;
+//      }
+//  }
+    public String toString () {
+         
+        String toReturn = "";
+        for (int i = leftSide; i < rightSide; i++) {
+            toReturn += " " + dominoes[i];
+        }
+         
+        return toReturn;
+    }
+     
 }
