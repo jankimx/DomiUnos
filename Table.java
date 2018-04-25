74ac8a4ed94536297c3361a55d7b3302e6da1180
jankimx <36344199+jankimx@users.noreply.github.com>
2018-04-25 00:01:26 -0400

    updated table so that it can work for left or right

--- Table.java
+++ Table.java
@@ -7,14 +7,74 @@ public class Table {
     int leftSide  = 26;
      
     //Dominoes currently on table, not in order
-    public boolean addOnTable (Dominoes toAdd) {
+    public String addOnTable (Dominoes toAdd) {
         if (numDoms == 0) {
         dominoes[27] = toAdd;
         right = dominoes[27].botside;
         left = dominoes[27].topside;
         numDoms++;
-        return true;
-        } else { //If the dominoes topside is equal to the outmost right tile, then we add that
+        return "any";
+        }
+        else if (numDoms == 1) {
+        	if (toAdd.topside == dominoes[27].botside || toAdd.topside == dominoes[27].topside) {
+        		dominoes[rightSide] = toAdd;
+        		right = toAdd.botside;
+        		rightSide ++;
+        		numDoms ++;
+        		toAdd.last = "rightbotside";
+        		return "rightbotside";
+        	}
+        	else if (toAdd.botside == dominoes[27].botside || toAdd.botside == dominoes[27].topside) {
+        		dominoes[rightSide] = toAdd;
+        		right = toAdd.topside;
+        		rightSide ++;
+        		numDoms ++;
+        		toAdd.last = "righttopside";
+        		return "righttopside";
+        	}
+        	else {
+        		return "nothing";
+        	}
+        }
+        else if (numDoms == 2) {
+        	if (toAdd.topside == dominoes[27].botside || toAdd.topside == dominoes[27].topside) {
+        		dominoes[leftSide] = toAdd;
+        		left = toAdd.botside;
+        		leftSide ++;
+        		numDoms ++;
+        		toAdd.last = "leftbotside";
+        		return "leftbotside";
+        	}
+        	else if (toAdd.botside == dominoes[27].botside || toAdd.botside == dominoes[27].topside) {
+        		dominoes[leftSide] = toAdd;
+        		left = toAdd.topside;
+        		leftSide ++;
+        		numDoms ++;
+        		toAdd.last = "lefttopside";
+        		return "lefttopside";
+        	}
+        	else if (toAdd.topside == right) {
+                dominoes[rightSide] = toAdd;
+                right = toAdd.botside;
+                rightSide++;
+                numDoms++;
+                toAdd.last = "rightbotside";
+                return "rightbotside";
+            }
+            else if(toAdd.botside == right) {
+                dominoes[rightSide] = toAdd;
+                right = toAdd.topside;
+                rightSide++;
+                numDoms++;
+                toAdd.last = "righttopside";
+                return "righttopside";
+            }
+            else {
+            	return "nothing";
+            }
+        		
+        }
+        else { //If the dominoes topside is equal to the outmost right tile, then we add that
                 // domino on the right side of the array and make right equal to botside
                 // we run the same idea on the other 3 cases
             if (toAdd.topside == right) {
@@ -22,31 +82,35 @@ public class Table {
                 right = toAdd.botside;
                 rightSide++;
                 numDoms++;
-                return true;
+                toAdd.last = "rightbotside";
+                return "rightbotside";
             }
             else if(toAdd.botside == right) {
                 dominoes[rightSide] = toAdd;
                 right = toAdd.topside;
                 rightSide++;
                 numDoms++;
-                return true;
+                toAdd.last = "righttopside";
+                return "righttopside";
             }
             else if(toAdd.topside == left) {
                 dominoes[leftSide] = toAdd;
                 left = toAdd.botside;
                 leftSide--;
                 numDoms++;
-                return true;
+                toAdd.last = "leftbotside";
+                return "leftbotside";
             }
             else if(toAdd.botside == left) {
                 dominoes[leftSide] = toAdd;
                 right = toAdd.topside;
                 leftSide--;
                 numDoms++;
-                return true;
+                toAdd.last = "lefttopside";
+                return "lefttopside";
             }
             else {
-            	return false;
+            	return "nothing";
             }
         }
     }
